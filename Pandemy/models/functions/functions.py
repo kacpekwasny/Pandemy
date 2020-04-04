71d3f4a5d3f05f5ce5d08c7e9697e3a4c1eb26a8
Kacper Kwasny <kwasny.kg@gmail.com>
2020-04-04 16:16:48 +0200

    Troche w person, person_config i person_instruction

--- Pandemy/models/functions/functions.py
+++ Pandemy/models/functions/functions.py
@@ -1,3 +1,4 @@
+from math import sqrt
 # Takes dict, dict
 # Returns overlap of them 
 def full_config(conf, def_conf):
@@ -11,3 +12,9 @@ def full_config(conf, def_conf):
             print("update", i)
             ret.update([(i, z)])
     return ret
+
+def away_from(_self, other):
+    ox, oy = other.cords
+    sx, sy = _self.cords
+    l = sqrt((ox-sx)**2 + (oy-sy)**2)
+    return l
--- Pandemy/models/person.py
+++ Pandemy/models/person.py
@@ -21,21 +21,53 @@
 #  
 #  
 #  
-from functions.functions import full_config
+# from functions.functions import full_config
+# nie uzywany import aktualnie
 # full config bierze dict config, i dict default_config,
 # zwraca dict jako uzupelnienie config przez default config :) 
 
+from functions.functions import away_from
+from ..config_and_instructions.person_config import disease_config
 
 class Person:
     # kacper = Person({"pid":1, "age":17, "infected":True, "symptoms":False}, {"pid":0, "age":30, "infected":False, "symptoms":False, "config":"yes"})
     # config odpowiada za wprowadzenie danych, przygotowane configi dla mlodych starych itd..
     # Tym sposobem mozna wprowadzic ile sie chce atrybotow
     # CZYLI BARDZO BARDZO skalowalne!!!
-    def __init__(self, config, def_config):
-        self_config = full_config(config, def_config)
-        for i, z in self_config.items():
+    def __init__(self, config):
+        # type(config) = dict !!!
+        self.pid = None
+
+        # Atrybuty personalne majace znaczenie dla zarazania
+        #    
+        self.age = None             # wiek 
+        self.infected = False       # zarazony, umycie rak nic nie da
+        self.time_infected = 0      # czas od zarazenia, po X czasu pojawiaja sie symptomy albo i nie 
+        self.symptoms = False       # czy ma symptomy 
+        self.cords = (0.00, 0.00)   # koordynaty
+                                    #  
+        self.infecting = False      # czy zaraza? 
+        self.dirty = 0              # czy jest zabrudzdona wirusami to moz 
+        self.dirty_hands = False    # czy ma brudne rece? bedzie zakazala, klamki itp.: wykorzystaj to do swojej symulacji
+        self.death_chance = None    # szansa na smierc w zaleznosci od wieku i ewentualnych powiklan zdrowotnych 
+
+        # te atrybuty sa od  
+        self.home = None            # dom jako obiekt
+        self.day_place = None       # miejsce gdzie sie na ogol w dzien siedzi    
+        self.day_plan_array = []
+        self.time_in_place = 0
+        self.max_time_in_place = None
+        for i, z in config.items():
             setattr(self, i, z)
 
+    def __repr__(self):
+        return "pid: {}, age: {}, infected: {}, death chance: {}".format(self.pid, self.age, self.infected, self.death_chance)
+
+    
+
+    def infect_in_radius(self, other):
+        if self.infected and away_from(self, otther)<:
+            if random()
 
     # kiedy bedziemy robili symulacje miejsc pracy itd 
     def move(self, **options):
